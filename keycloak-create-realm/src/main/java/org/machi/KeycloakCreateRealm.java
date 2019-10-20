5c58a9d27b981162c7eb9ae609094e5d52c45c70
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2019-10-20 13:40:26 +0200

    feat(keycloak): add realm creation project

---
+++ keycloak-create-realm/src/main/java/org/machi/KeycloakCreateRealm.java
@@ -0,0 +1,210 @@
+package org.machi;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import org.keycloak.representations.AccessTokenResponse;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.RolesRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.idm.authorization.PolicyRepresentation;
+import org.keycloak.representations.idm.authorization.ResourceRepresentation;
+import org.keycloak.representations.idm.authorization.ResourceServerRepresentation;
+import org.keycloak.util.JsonSerialization;
+
+import io.restassured.RestAssured;
+
+public class KeycloakCreateRealm {
+    public static void main(String[] args) throws IOException {
+        configureKeycloakRealm();
+    }
+
+    private static final String KEYCLOAK_SERVER_URL = System.getProperty("keycloak.url", "http://localhost:8180/auth");
+    private static final String KEYCLOAK_REALM = "todo-api";
+
+    public static void configureKeycloakRealm() throws IOException {
+        RealmRepresentation realm = createRealm(KEYCLOAK_REALM);
+
+        realm.getClients().add(createClient("machi-todo-apip"));
+        realm.getUsers().add(createUser("alice", "user"));
+        realm.getUsers().add(createUser("admin", "user", "admin"));
+        realm.getUsers().add(createUser("jdoe", "user", "confidential"));
+
+        RestAssured
+                .given()
+                .auth().oauth2(getAdminAccessToken())
+                .contentType("application/json")
+                .body(JsonSerialization.writeValueAsBytes(realm))
+                .when()
+                .post(KEYCLOAK_SERVER_URL + "/admin/realms").then()
+                .statusCode(201);
+    }
+
+    public static void removeKeycloakRealm() {
+        RestAssured
+                .given()
+                .auth().oauth2(getAdminAccessToken())
+                .when()
+                .delete(KEYCLOAK_SERVER_URL + "/admin/realms/" + KEYCLOAK_REALM).then().statusCode(204);
+    }
+
+    private static String getAdminAccessToken() {
+        return RestAssured
+                .given()
+                .param("grant_type", "password")
+                .param("username", "admin")
+                .param("password", "admin")
+                .param("client_id", "admin-cli")
+                .when()
+                .post(KEYCLOAK_SERVER_URL + "/realms/master/protocol/openid-connect/token")
+                .as(AccessTokenResponse.class).getToken();
+    }
+
+    private static RealmRepresentation createRealm(String name) {
+        RealmRepresentation realm = new RealmRepresentation();
+
+        realm.setRealm(name);
+        realm.setEnabled(true);
+        realm.setUsers(new ArrayList<>());
+        realm.setClients(new ArrayList<>());
+
+        RolesRepresentation roles = new RolesRepresentation();
+        List<RoleRepresentation> realmRoles = new ArrayList<>();
+
+        roles.setRealm(realmRoles);
+        realm.setRoles(roles);
+
+        realm.getRoles().getRealm().add(new RoleRepresentation("user", null, false));
+        realm.getRoles().getRealm().add(new RoleRepresentation("admin", null, false));
+        realm.getRoles().getRealm().add(new RoleRepresentation("confidential", null, false));
+
+        return realm;
+    }
+
+    private static ClientRepresentation createClient(String clientId) {
+        ClientRepresentation client = new ClientRepresentation();
+
+        client.setClientId(clientId);
+        client.setPublicClient(false);
+        client.setSecret("secret");
+        client.setDirectAccessGrantsEnabled(true);
+        client.setEnabled(true);
+
+        client.setAuthorizationServicesEnabled(true);
+
+        ResourceServerRepresentation authorizationSettings = new ResourceServerRepresentation();
+
+        authorizationSettings.setResources(new ArrayList<>());
+        authorizationSettings.setPolicies(new ArrayList<>());
+
+        configureConfidentialResourcePermission(authorizationSettings);
+        configurePermissionResourcePermission(authorizationSettings);
+
+        client.setAuthorizationSettings(authorizationSettings);
+
+        return client;
+    }
+
+    private static void configureConfidentialResourcePermission(ResourceServerRepresentation authorizationSettings) {
+        ResourceRepresentation resource = new ResourceRepresentation("Confidential Resource");
+
+        resource.setUris(Collections.singleton("/api/confidential"));
+
+        authorizationSettings.getResources().add(resource);
+
+        PolicyRepresentation policy = new PolicyRepresentation();
+
+        policy.setName("Confidential Policy");
+        policy.setType("js");
+        policy.setConfig(new HashMap<>());
+        policy.getConfig().put("code",
+                "var identity = $evaluation.context.identity;\n" +
+                        "\n" +
+                        "if (identity.hasRealmRole(\"confidential\")) {\n" +
+                        "$evaluation.grant();\n" +
+                        "}");
+
+        authorizationSettings.getPolicies().add(policy);
+
+        PolicyRepresentation permission = new PolicyRepresentation();
+
+        permission.setName("Confidential Permission");
+        permission.setType("resource");
+        permission.setResources(new HashSet<>());
+        permission.getResources().add(resource.getName());
+        permission.setPolicies(new HashSet<>());
+        permission.getPolicies().add(policy.getName());
+
+        authorizationSettings.getPolicies().add(permission);
+    }
+
+    private static void configurePermissionResourcePermission(ResourceServerRepresentation authorizationSettings) {
+        ResourceRepresentation resource = new ResourceRepresentation("Permission Resource");
+
+        resource.setUris(Collections.singleton("/api/permission"));
+
+        authorizationSettings.getResources().add(resource);
+
+        PolicyRepresentation policy = new PolicyRepresentation();
+
+        policy.setName("Permission Policy");
+        policy.setType("js");
+        policy.setConfig(new HashMap<>());
+        policy.getConfig().put("code", "$evaluation.grant();");
+
+        authorizationSettings.getPolicies().add(policy);
+
+        PolicyRepresentation permission = new PolicyRepresentation();
+
+        permission.setName("Permission Resource Permission");
+        permission.setType("resource");
+        permission.setResources(new HashSet<>());
+        permission.getResources().add(resource.getName());
+        permission.setPolicies(new HashSet<>());
+        permission.getPolicies().add(policy.getName());
+
+        authorizationSettings.getPolicies().add(permission);
+    }
+
+    private static UserRepresentation createUser(String username, String... realmRoles) {
+        UserRepresentation user = new UserRepresentation();
+
+        user.setUsername(username);
+        user.setEnabled(true);
+        user.setCredentials(new ArrayList<>());
+        user.setRealmRoles(Arrays.asList(realmRoles));
+
+        CredentialRepresentation credential = new CredentialRepresentation();
+
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setValue(username);
+        credential.setTemporary(false);
+
+        user.getCredentials().add(credential);
+
+        return user;
+    }
+
+
+    private String getAccessToken(String userName) {
+        return RestAssured
+                .given()
+                .param("grant_type", "password")
+                .param("username", userName)
+                .param("password", userName)
+                .param("client_id", "quarkus-app")
+                .param("client_secret", "secret")
+                .when()
+                .post(KEYCLOAK_SERVER_URL + "/realms/" + KEYCLOAK_REALM + "/protocol/openid-connect/token")
+                .as(AccessTokenResponse.class).getToken();
+    }
+}
