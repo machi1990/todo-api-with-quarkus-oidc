3923216189979a4d58534ff1f7780aeec9a082a5
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2019-11-02 12:22:11 +0100

    docs: add some description in OpenAPI definition

--- machi-todo-api/src/main/java/org/machi/api/TodoApplication.java
+++ machi-todo-api/src/main/java/org/machi/api/TodoApplication.java
@@ -1,8 +1,18 @@
 package org.machi.api;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
+@OpenAPIDefinition(info = @Info(
+        title = "TODO API With Quarkus OIDC",
+        version = "1.0.0", contact = @Contact(
+                name = "Manyanda Chitimbo",
+        email = "manyanda.chitimbo@gmail.com"
+)))
 public class TodoApplication extends Application {
 }
